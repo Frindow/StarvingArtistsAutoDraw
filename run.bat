ad58344b564d5190bacde42c37f7f98b8f61aaed
Frindow <69374353+Frindow@users.noreply.github.com>
2025-03-01 22:54:13 -0500

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+echo ==================================
+echo Starting...
+echo ==================================
+python main.py
+echo ==================================
+echo Execution finished.
+echo Press any key to exit...
+pause >nul
